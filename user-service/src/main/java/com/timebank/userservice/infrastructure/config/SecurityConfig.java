75d8c6e1fff9dacd5ea76e74535a7bc52eb7a36f
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-04-07 15:53:53 +0900

    fixing

---
+++ user-service/src/main/java/com/timebank/userservice/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.timebank.userservice.infrastructure.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.httpBasic(httpBasic -> httpBasic.disable())
+			.formLogin(formLogin -> formLogin.disable())
+			.authorizeHttpRequests(authrize -> authrize
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+				.requestMatchers("/api/auth/signup").permitAll()
+				.requestMatchers("/api/auth/login").permitAll()
+				.requestMatchers("/api/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			);
+		return http.build();
+	}
+}
