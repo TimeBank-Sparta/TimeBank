9fcc556dc5ccba93e30f30caa2a79c7b04ca4ffd
yng <namgyu967@gmail.com>
2025-04-08 12:26:18 +0900

    - feat : Create config
     - Create config : config 서버 생성

---
+++ config/src/test/java/com/timebank/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timebank.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
