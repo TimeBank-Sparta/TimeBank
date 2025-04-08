ff8dc0d05d004f4754c0016ac649a79d3026dd62
letsgilit <wjdrlf5781@naver.com>
2025-04-08 14:11:01 +0900

    build: yml파일 작성 -gateway 설정 route추가 -help-service추가

---
+++ help-service/src/test/java/com/timebank/helpservice/HelpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timebank.helpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
