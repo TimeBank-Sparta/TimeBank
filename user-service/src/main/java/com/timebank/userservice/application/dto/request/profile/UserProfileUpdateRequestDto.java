baed64b98f109205c69a7363cbd0d604c607ce08
s_nowsssㄴㄴㄴㄴsassssjgi2447a <howlarang123@naver.com>
2025-04-09 14:52:28 +0900

    Feat : 프로필 관련 DTO 구현

---
+++ user-service/src/main/java/com/timebank/userservice/application/dto/request/profile/UserProfileUpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.timebank.userservice.application.dto.request.profile;
+
+import java.util.Set;
+
+import com.timebank.userservice.domain.model.profile.ServiceCategory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileUpdateRequestDto {
+
+	private String nickname;
+	private Set<ServiceCategory> helpServices;
+	private Set<ServiceCategory> needServices;
+	private String location;
+	private String introduction;
+}
---
+++ user-service/src/main/java/com/timebank/userservice/application/dto/response/profile/UserProfileResponseDto.java
@@ -0,0 +1,32 @@
+package com.timebank.userservice.application.dto.response.profile;
+
+import java.util.Set;
+
+import com.timebank.userservice.domain.model.profile.ServiceCategory;
+import com.timebank.userservice.domain.model.profile.UserProfile;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserProfileResponseDto {
+	private Long id;
+	private String nickname;
+	private Set<ServiceCategory> helpServices;
+	private Set<ServiceCategory> needServices;
+	private String location;
+	private String introduction;
+	//todo: 포인트도 추가해야함
+
+	public static UserProfileResponseDto from(UserProfile profile) {
+		return UserProfileResponseDto.builder()
+			.id(profile.getId())
+			.nickname(profile.getNickname())
+			.helpServices(profile.getHelpServices())
+			.needServices(profile.getNeedServices())
+			.location(profile.getLocation())
+			.introduction(profile.getIntroduction())
+			.build();
+	}
+}
