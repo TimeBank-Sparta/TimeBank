58b8def2e35c1c8185b36de9ddbeab79c4f6529b
letsgilit <wjdrlf5781@naver.com>
2025-04-10 15:30:17 +0900

    feat: 도메인 분리
    -기존 도메인 제거

--- help-service/src/main/java/com/timebank/helpservice/presentation/dto/request/CreateTradingRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.timebank.helpservice.presentation.dto.request;
-
-public record CreateTradingRequestDto(
-	Long helpRequestId,
-	Long requesterId,
-	Long helperId,
-	int actualPoints
-) {
-}
