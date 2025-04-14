fcca8cf43af6fdeb29000d5941225cf3149808f0
yng <namgyu967@gmail.com>
2025-04-14 12:22:13 +0900

    - fix : review
     - review : 리뷰 도메인 누락 파일

---
+++ review-service/src/test/java/com/timebank/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timebank.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ReviewApplication.class)
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
