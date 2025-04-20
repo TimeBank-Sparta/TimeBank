9affc295d20f75576613fcb1724d38827f2e112a
yng <namgyu967@gmail.com>
2025-04-20 19:50:21 +0900

    - fix : KafkaConfig
     - KafkaConfig : 카프카 설정 오타 수정

--- common/src/main/java/com/timebank/common/infrastructure/config/KafkaConfig.java
+++ common/src/main/java/com/timebank/common/infrastructure/config/KafkaConfig.java
@@ -25,7 +25,7 @@ public class KafkaConfig {
 	@Bean
 	public ProducerFactory<String, Object> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kakfa:9092");
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
@@ -34,7 +34,7 @@ public class KafkaConfig {
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kakfa:9092");
+		configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 		return new DefaultKafkaConsumerFactory<>(configProps);
