d6ac3804d4b8ce6f7130518e4ba62d0713d7289c
yng <namgyu967@gmail.com>
2025-04-25 10:03:14 +0900

    - feat : FeignConfig
     - FeignConfig : retryer 파라미터 조정

--- common/src/main/java/com/timebank/common/infrastructure/config/FeignConfig.java
+++ common/src/main/java/com/timebank/common/infrastructure/config/FeignConfig.java
@@ -15,7 +15,8 @@ public class FeignConfig {
 
 	@Bean
 	public Retryer retryer() {
-		return new Retryer.Default(100L, SECONDS.toMillis(1L), 5); // 실패 시 최대 5번까지 재시도
+		// 0.1초의 간격으로 시작해 최대 1초의 간격으로 점점 증가하며, 최대5번 재시도한다.
+		return new Retryer.Default(100L, SECONDS.toMillis(1L), 5);
 	}
 
 	@Bean
